2c434b12641e85388ad161326a616887932aa501
Rônel de Souza Dornas <roneldornas@hotmail.com>
2025-10-06 08:23:56 -0300

    first commit

---
+++ src/reports/report.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+
+  // @Column()
+  // make: string;
+
+  // @Column()
+  // model: string;
+
+  // @Column()
+  // year: number;
+}
